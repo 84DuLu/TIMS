82190b820cda52d19aa051ded10f1e143b176518
84DuLu <knight36999@163.com>
2023-12-22 09:59:24 +0800

    创建登录页面,增加登录逻辑

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import click
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://snowy:Snowy_77@localhost/tims"
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     password_hash = db.Column(db.String(128))
@@ -18,7 +21,11 @@ class User(db.Model):
 
     def validate_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.cli.command()
 @click.option('--username', prompt=True, help='The username used to login.')
 @click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
@@ -40,4 +47,21 @@ def signup(username, password):
 
 @app.route('/')
 def index():
-    return 'hello'
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.validate_password(password):
+            login_user(user)
+            return redirect(url_for('index'))
+
+        flash('用户名重复或密码错误')
+        return redirect(url_for('login'))
+
+    return render_template('login.html')

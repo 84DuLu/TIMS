5842ed60b4000cca3b70a627adffbd4932ea0c9a
84DuLu <knight36999@163.com>
2023-12-24 09:37:23 +0800

    增加养护信息页面以及其相关功能

--- app.py
+++ app.py
@@ -84,16 +84,27 @@ class MaintainInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tunnel_name = db.Column(db.String(20), nullable=False, index=True)
     check_program = db.Column(db.String(20), nullable=False)
-    check_time = db.Column(db.DateTime, nullable=False, index=True)
+    check_time = db.Column(db.String(20), nullable=False, index=True)
     check_name = db.Column(db.String(20))
     enter_man = db.Column(db.String(20))
     conclusion = db.Column(db.String(100))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'tunnel_name': self.tunnel_name,
+            'check_program': self.check_program,
+            'check_time': self.check_time,
+            'check_name': self.check_name,
+            'enter_man': self.enter_man,
+            'conclusion': self.conclusion
+        }
 
 @app.cli.command()
 def forge():
     db.create_all()
 
-    maintain_info = [
+    tunnel_info = [
     {'number': 'G69U000610000', 'name': '秦岭终南山隧道', 'length': 22110, 'province': '陕西省', 
      'lane': 2, 'year': 2007, 'highway': '银百高速'},
     {'number': 'G85U000610000', 'name': '秦岭天台山隧道', 'length': 15560, 'province': '陕西省', 
@@ -108,15 +119,25 @@ def forge():
      'lane': 2, 'year': 2013, 'highway': '安长高速'}
     ]
 
-    for info in maintain_info:
+    for info in tunnel_info:
         item = TunnelInfo(number=info['number'], name=info['name'], length=info['length'],
                           province=info['province'], lane=info['lane'], year=info['year'],
                           highway=info['highway'])
         db.session.add(item)
 
+    maintain_info = [
+        {'tunnel_name': '秦岭终南山隧道', 'check_program': '洞口', 'check_time': '2017-06-01T08:30',
+         'check_name': '长安大学', 'enter_man': '王二', 'conclusion': '洞口边仰坡稳定、无落石'}
+    ]
+
+    for info in maintain_info:
+        item = MaintainInfo(tunnel_name=info['tunnel_name'], check_program=info['check_program'],
+                            check_time=info['check_time'], check_name=info['check_name'],
+                            enter_man=info['enter_man'], conclusion=info['conclusion'])
+        db.session.add(item)
+
     db.session.commit()
     click.echo('Done.')
-
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -142,12 +163,21 @@ def signup(username, password):
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/maintain')
+def maintain():
+    return render_template('maintain.html')
 
 @app.route('/api/data')
 def data():
     return {'data': [info.to_dict() for info in TunnelInfo.query]}
+
+@app.route('/api/data2')
+def data2():
+    return {'data': [info.to_dict() for info in MaintainInfo.query]}
 
 @app.route('/tunnel/add', methods=['GET', 'POST'])
 def add():
@@ -167,6 +197,23 @@ def add():
         return redirect(url_for('index'))
     
     return render_template('add.html', provinces=provinces)
+
+@app.route('/maintain/add', methods=['GET', 'POST'])
+def maintain_add():
+    if request.method == 'POST':
+        tunnel_name = request.form['tunnel_name']
+        check_program = request.form['check_program']
+        check_time = request.form['check_time']
+        check_name = request.form['check_name']
+        enter_man = request.form['enter_man']
+        conclusion = request.form['conclusion']
+
+        maintain = MaintainInfo(tunnel_name=tunnel_name, check_program=check_program, check_time=check_time,
+                                check_name=check_name, enter_man=enter_man, conclusion=conclusion)
+        db.session.add(maintain)
+        db.session.commit()
+        return redirect(url_for('maintain'))
+    return render_template('maintain_add.html')
 
 @app.route('/tunnel/edit/<int:tunnel_id>', methods=['GET', 'POST'])
 def edit(tunnel_id):
@@ -193,12 +240,42 @@ def edit(tunnel_id):
 
     return render_template('edit.html', provinces=provinces, tunnel=tunnel)
 
+@app.route('/maintain/edit/<int:maintain_id>', methods=['GET', 'POST'])
+def maintain_edit(maintain_id):
+    maintain = MaintainInfo.query.get_or_404(maintain_id)
+
+    if request.method == 'POST':
+        tunnel_name = request.form['tunnel_name']
+        check_program = request.form['check_program']
+        check_time = request.form['check_time']
+        check_name = request.form['check_name']
+        enter_man = request.form['enter_man']
+        conclusion = request.form['conclusion']
+
+        maintain.tunnel_name = tunnel_name
+        maintain.check_program = check_program
+        maintain.check_time = check_time
+        maintain.check_name = check_name
+        maintain.enter_man = enter_man
+        maintain.conclusion = conclusion
+        db.session.commit()
+        return redirect(url_for('maintain'))
+    
+    return render_template('maintain_edit.html', maintain=maintain)
+
 @app.route('/tunnel/delete/<int:tunnel_id>', methods=['POST'])
 def delete(tunnel_id):
     tunnel = TunnelInfo.query.get_or_404(tunnel_id)
     db.session.delete(tunnel)
     db.session.commit()
     return redirect(url_for('index'))
+
+@app.route('/maintain/delete/<int:maintain_id>', methods=['POST'])
+def maintain_delete(maintain_id):
+    maintain = MaintainInfo.query.get_or_404(maintain_id)
+    db.session.delete(maintain)
+    db.session.commit()
+    return redirect(url_for('maintain'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
---
+++ static/maintain.js
@@ -0,0 +1,64 @@
+new gridjs.Grid({
+    columns: [
+        {id: 'id', hidden: true},
+        {id: 'tunnel_name', name: '隧道名称'},
+        {id: 'check_program', name: '检查项目'},
+        {id: 'check_time', name: '检查时间'},
+        {id: 'check_name', name: '检查公司', sort: false},
+        {id: 'enter_man', name: '录入人员', sort: false},
+        {id: 'conclusion', name: '检查结论', sort: false},
+        {
+            sort: false,
+            formatter: (_, row) => gridjs.html(`<a 
+            class="btn btn-primary" 
+            href="/maintain/edit/${maintain_id=row.cells[0].data}" 
+            role="button">编辑</a>
+            <form class="inline-form" method="post" action="/maintain/delete/${maintain_id=row.cells[0].data}">
+                <input class="btn btn-danger" type="submit" value="删除" onclick="return confirm('确定删除吗?')">
+            </form>`)
+        },
+    ],
+    server: {
+        url: '/api/data2',
+        then: results => results.data,
+    },
+    language: {
+        search: {
+            placeholder: "输入关键字..."
+        },
+        sort: {
+            sortAsc: "升序排列",
+            sortDesc: "降序排列"
+        },
+        pagination: {
+            previous: "上一页",
+            next: "下一页",
+            navigate: function(e, r) {
+                return "第" + e + "页，共" + r + "页"
+            },
+            page: function(e) {
+                return "第" + e + "页"
+            },
+            showing: "第",
+            of: "到",
+            to: "条记录，共",
+            results: "条"
+        },
+        loading: "玩命加载中...",
+        noRecordsFound: "没找到匹配的项",
+        error: "获取数据时发生了错误"
+    },
+    search: true,
+    sort: true,
+    pagination: true,
+    }).render(document.getElementById('table'));
+
+const head = document.querySelector('.gridjs-head');
+
+const addBtn = document.createElement('a');
+addBtn.className = 'btn btn-dark';
+addBtn.href = '/maintain/add';
+addBtn.textContent = '添加新条目';
+addBtn.style.marginLeft = '1rem';
+
+head.appendChild(addBtn)
